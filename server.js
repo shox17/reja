25567f10b2d22fe9956336e8bc847b7d5d4abec9
shox17 <159898318+shox17@users.noreply.github.com>
2025-09-18 17:05:53 +0900

    feat: develop author router page

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log("ERROR: ", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});    
 
 // 1: Entering code
 app.use(express.static('public'));
@@ -20,6 +30,9 @@ app.post("/create-item", function(req, res) {
     res.json({ message: "Item created successfully" });
 });
 
+app.get("/author", function(req, res) {
+    res.render("author", { user: user });
+});
 app.get("/", function(req, res) {
     res.render("purchase");
 });
---
+++ views/author.ejs
@@ -0,0 +1,1 @@
+<H1>hELLO wORLD</H1>
